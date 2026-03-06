14e0ff45d5c8fd99dbe558ff945d5c42a3e36e52
Marco Dominguez <210174564+Marcodggz@users.noreply.github.com>
2026-03-06 18:21:50 +0100

    feat: move playlist and track data to React state and add basic layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import SearchResults from './components/SearchResults/SearchResults'
 import Playlist from './components/Playlist/Playlist'
 
 function App() {
-  const tracks = [
+
+  const [tracks, setTracks] = useState([
   { id: 1, name: "Poker Face", artist: "Lady Gaga", album: "The Fame" },
   { id: 2, name: "Bad Romance", artist: "Lady Gaga", album: "The Fame Monster" },
   { id: 3, name: "Just Dance", artist: "Lady Gaga", album: "The Fame" }
-  ]
-
-  const playlistName = "My Playlist";
-  const playlistTracks = [
+  ]);
+  const [playlistName, setPlaylistName] = useState("My Playlist"); 
+  const [playlistTracks, setPlaylistTracks] = useState([
     { id: 1, name: "Soy Peor", artist: "Bad Bunny", album: "X 100PRE" },
     { id: 2, name: "DÁKITI", artist: "Bad Bunny", album: "El Último Tour Del Mundo" },
     { id: 3, name: "La Canción", artist: "Bad Bunny", album: "YHLQMDLG" },
     { id: 4, name: "Monaco", artist: "Bad Bunny", album: "El Último Tour Del Mundo" },
     { id: 5, name: "Ella y Yo", artist: "Bad Bunny", album: "X 100PRE" }
-  ];
+  ]);
 
   return (
     <>
@@ -29,17 +29,19 @@ function App() {
             <button>Search</button>
           </div>
         </div>
-        <div className="results">
-          <div className="searchResults">
-            <SearchResults tracks={tracks} /> 
+        <div className='mainContainer'>
+          <div className="results">
+            <div className="searchResults">
+              <SearchResults tracks={tracks} /> 
+            </div> 
           </div> 
-        </div> 
-        <div className="playlist">
-          <div>
-            <Playlist playlistName={playlistName} playlistTracks={playlistTracks} />
+          <div className="playlist">
+            <div>
+              <Playlist playlistName={playlistName} playlistTracks={playlistTracks} />
+            </div>
           </div>
         </div>
-        <div>
+        <div className='saveContainer'>
           <button>Save To Spotify</button>
         </div>
       </div>
