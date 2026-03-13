d207adf63549c2e32229fe7006375cc8345ff085
Marco Dominguez <210174564+Marcodggz@users.noreply.github.com>
2026-03-13 16:01:28 +0100

    feat: add base responsive layout for tablet and mobile

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -3,10 +3,12 @@ import "./Playlist.css";
 
 function Playlist({playlistName, playlistTracks, showAddButton=false, removeTrack, showRemoveButton=true, playlistNameChange, savePlaylist}) {
   return (
-    <div id="playlist" className="playlistContainer">
+    <div id="playlistCard" className="playlistContainer">
       <input value={playlistName} onChange={playlistNameChange} />
       <TrackList tracks={playlistTracks} showAddButton={showAddButton} removeTrack={removeTrack} showRemoveButton={showRemoveButton} />
-      <button onClick={savePlaylist}>Save To Spotify</button>
+      <div className="saveContainer">
+        <button className="saveButton" onClick={savePlaylist}>Save To Spotify</button>
+      </div>
     </div>
   );
 }
